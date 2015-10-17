f5d43408a4338da301c60f92a00e2b6cf2427637
Jason King <jk1ng@yahoo.com>
2015-10-17 21:51:53 +0000

    Initial commit

---
+++ atp_input.c
@@ -0,0 +1,143 @@
+/*
+ * Add an AT packet to ATPs input queue
+ *
+ * $Id: atp_input.c,v 1.1.1.1 2001/10/28 15:01:48 stefanbethke Exp $
+ *
+ * Original work (c) 1997 Stefan Bethke. All rights reserved.
+ * Modified work (c) 2015 Jason King. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <sys/param.h>
+#include <netinet/in.h>
+#include <string.h>
+#undef s_net
+#include <netatalk/at.h>
+#include <atalk/ddp.h>
+#include <atalk/atp.h>
+#include <stdlib.h>
+#include "atp_internals.h"
+
+#ifdef EBUG
+#include <stdio.h>
+#endif
+
+extern int at_addr_eq (struct sockaddr_at *paddr, struct sockaddr_at *saddr);
+
+static int free_buf (struct atpbuf *bp) {
+	if (bp != NULL)
+		free(bp);
+	bp = NULL;
+	
+	return 0;
+}
+
+static void *alloc_buf (void) {
+	void *p;
+	
+	p = malloc(sizeof(struct atpbuf));
+	if(p != NULL)
+		return p;
+	else
+		return NULL;
+}
+
+/*#define EBUG
+*/
+
+int atp_input(ATP ah, struct sockaddr_at *faddr, char *rbuf, int recvlen) {
+	struct atpbuf		*pq, *cq;
+	struct atphdr		ahdr;
+	u_short				rfunc;
+	u_short				rtid;
+	int					i;
+	struct atpbuf		*inbuf;
+
+	bcopy( rbuf + 1, (char *)&ahdr, sizeof( struct atphdr ));
+	if ( recvlen >= ATP_HDRSIZE && *rbuf == DDPTYPE_ATP) {
+		/* this is a valid ATP packet -- check for a match */
+		rfunc = ahdr.atphd_ctrlinfo & ATP_FUNCMASK;
+		rtid = ahdr.atphd_tid;
+#ifdef EBUG
+		printf( "<%d> got tid=%hu func=", getpid(), ntohs( rtid ));
+		print_func( rfunc );
+		print_addr( " from", faddr );
+		putchar( '\n' );
+		bprint( rbuf, recvlen );
+#endif
+		if ( rfunc == ATP_TREL ) {
+			/* remove response from sent list */
+			for ( pq = NULL, cq = ah->atph_sent; cq != NULL;
+			  pq = cq, cq = cq->atpbuf_next ) {
+				if ( at_addr_eq( faddr, &cq->atpbuf_addr ) &&
+				  cq->atpbuf_info.atpbuf_xo.atpxo_tid == ntohs( rtid )) 
+					break;
+			}
+			if ( cq != NULL ) {
+#ifdef EBUG
+		printf( "<%d> releasing transaction %hu\n", getpid(), ntohs( rtid ));
+#endif
+				if ( pq == NULL ) {
+					ah->atph_sent = cq->atpbuf_next;
+				} else {
+					pq->atpbuf_next = cq->atpbuf_next;
+				}
+				for ( i = 0; i < 8; ++i ) {
+					if ( cq->atpbuf_info.atpbuf_xo.atpxo_packet[ i ]
+								!= NULL ) {
+						free_buf ( cq->atpbuf_info.atpbuf_xo.atpxo_packet[ i ] );
+					}
+				}
+				free_buf( cq );
+			}
+		} else {
+			/* add packet to incoming queue */
+#ifdef EBUG
+		printf( "<%d> queuing incoming...\n", getpid() );
+#endif
+			if (( inbuf = (struct atpbuf *)alloc_buf()) == NULL ) {
+#ifdef EBUG
+		printf( "<%d> can't alloc buffer\n", getpid() );
+#endif
+				return -1;
+			}
+			bcopy( (char *)faddr, (char *)&inbuf->atpbuf_addr,
+			  sizeof( struct sockaddr_at ));
+			inbuf->atpbuf_next = ah->atph_queue;
+			ah->atph_queue = inbuf;
+			inbuf->atpbuf_dlen = recvlen;
+			bcopy( (char *)rbuf,
+				(char *)inbuf->atpbuf_info.atpbuf_data, recvlen );
+		}
+		return 0;
+	}
+
+	return -1; /* invalid packet */
+}
+
+
---
+++ tunnel_linux.c
@@ -0,0 +1,255 @@
+/*
+ * AppleTalk MacIP Gateway
+ *
+ * $Id: macip.c,v 1.1.1.1 2001/10/28 15:01:49 stefanbethke Exp $
+ *
+ * Original work (c) 1997 Stefan Bethke. All rights reserved.
+ * Modified work (c) 2015 Jason King. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
+#include <sys/errno.h>
+#include <netinet/in_systm.h>
+#include <netinet/in.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <linux/route.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "common.h"
+#include "util.h"
+#include "tunnel.h"
+
+struct tunnel {
+	int		dev;
+	char	name[32];
+	u_long	net;
+	u_long	mask;
+};
+
+static struct tunnel gTunnel;
+
+static outputfunc_t	gOutput;
+
+static void set_sin (struct sockaddr *s, u_long ip) {
+	struct sockaddr_in *sin = (struct sockaddr_in *)s;
+	bzero (sin, sizeof (*sin));
+	sin->sin_family      = AF_INET;
+	sin->sin_addr.s_addr = htonl(ip);
+}
+
+static int tunnel_ifconfig (void) {
+	int					sk;
+	struct ifreq		ifrq;
+
+	printf ("device = %s\n", gTunnel.name);
+	
+	sk = socket (PF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (sk < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_ifconfig: socket");
+		return -1;
+	}
+
+	bzero (&ifrq, sizeof (ifrq));
+	strncpy (ifrq.ifr_name,  gTunnel.name, IFNAMSIZ);
+	if (ioctl (sk, SIOCGIFFLAGS, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_ifconfig: SIOCGIFFLAGS");
+		close (sk);
+		return -1;
+	}
+
+	ifrq.ifr_flags |= IFF_UP;
+	if (ioctl (sk, SIOCSIFFLAGS, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_ifconfig: SIOCSIFFLAGS");
+		close (sk);
+		return -1;
+	}
+
+	ifrq.ifr_mtu = 586;
+	if (ioctl (sk, SIOCSIFMTU, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_ifconfig: SIOCSIFMTU");
+		close (sk);
+		return -1;
+	}
+
+	set_sin (&ifrq.ifr_addr,      gTunnel.net+1);
+	if (ioctl (sk, SIOCSIFADDR, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_ifconfig: SIOCSIFADDR");
+		close (sk);
+		return -1;
+	}
+
+	set_sin (&ifrq.ifr_addr,      gTunnel.mask);
+	if (ioctl (sk, SIOCSIFNETMASK, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_ifconfig: SIOCSIFNETMASK");
+		close (sk);
+		return -1;
+	}
+	
+	if (gDebug & DEBUG_TUNNEL) {
+		printf ("tunnel_ifconfig: setting address %s -> ", iptoa(gTunnel.net+1));
+		printf ("%s netmask ", iptoa(gTunnel.net));
+		printf ("%s\n", iptoa(gTunnel.mask));
+	}
+
+	close (sk);
+	return 0;
+}
+
+static int tunnel_create(char *dev, int flags) {
+
+	struct ifreq ifr;
+	int fd;
+	char *tundev = "/dev/net/tun";
+
+	if( (fd = open(tundev, O_RDWR)) < 0 ) {
+		return fd;
+	}
+
+	memset(&ifr, 0, sizeof(ifr));
+	ifr.ifr_flags = flags;
+	strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+	if (ioctl(fd, TUNSETIFF, (void *) &ifr) < 0 ) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_alloc: TUNSETIFF");
+		close(fd);
+		return -1;
+	}
+#if 0	
+	if (ioctl(fd, TUNSETPERSIST, 1) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_alloc: TUNSETPERSIST");
+      close(fd);
+      return -1;
+    }
+#endif
+	strcpy(dev, ifr.ifr_name);
+
+	return fd;
+}
+
+int tunnel_open (u_long net, u_long mask, outputfunc_t o) {
+	int					i;
+	char				s[32], *q;
+	
+	gTunnel.dev = 0;
+	for (i=0; i<=9; i++) {
+		sprintf (s, "tun%d", i);
+		gTunnel.dev = tunnel_create (s, IFF_TUN | IFF_NO_PI);
+		if (gTunnel.dev)
+			break;
+	}
+	
+	if (gTunnel.dev < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_open: open");
+		return -1;
+	}
+
+	strcpy (gTunnel.name, s);
+	if (gDebug & DEBUG_TUNNEL) {
+		printf ("tunnel_open: %s:%s opened.\n", s, gTunnel.name);
+	}
+
+	gTunnel.net   = net;
+	gTunnel.mask  = mask;	
+
+	if (tunnel_ifconfig () < 0) {
+		return -1;
+	}
+
+	gOutput = o;
+
+	return gTunnel.dev;	
+}
+
+void tunnel_close (void) {
+	int		i, sk;
+	struct ifreq		ifrq;
+	struct sockaddr_in	*sin;
+
+	sk = socket (PF_INET, SOCK_DGRAM, 0);
+	if (sk < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_close: socket");
+		return;
+	}
+	
+	bzero (&ifrq, sizeof (ifrq));
+	strncpy (ifrq.ifr_name,  gTunnel.name, IFNAMSIZ);
+
+	if (ioctl (sk, SIOCGIFFLAGS, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_close: SIOCGIFFLAGS");
+		close (sk);
+		return;
+	}
+	ifrq.ifr_flags &= ~IFF_UP;
+	if (ioctl (sk, SIOCSIFFLAGS, &ifrq) < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_close: SIOCSIFFLAGS");
+		close (sk);
+		return;
+	}
+
+	close (gTunnel.dev);
+}
+
+
+void tunnel_input (void) {
+	char	buffer[600];
+	int		i;
+	
+	i = read (gTunnel.dev, buffer, sizeof (buffer));
+	if (i < 0) {
+		if (gDebug & DEBUG_TUNNEL)
+			perror ("tunnel_input: read");
+		return;
+	}
+	if (gDebug & DEBUG_TUNNEL)
+		printf ("read packet from tunnel.\n");
+	gOutput (buffer, i);
+}
+
+
+void tunnel_output (char *buffer, int len) {
+	write (gTunnel.dev, buffer, len);
+	if (gDebug & DEBUG_TUNNEL)
+		printf ("sent packet into tunnel.\n");
+}
